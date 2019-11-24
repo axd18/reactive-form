b7a4158be047726f707f445374f94b878f4eff94
adrijfer18 <adrianfernandezj@gmail.com>
2019-11-24 19:23:09 -0300

    Despliegue a producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
    production: false,
    firebaseConfig : {
-    apiKey: "AIzaSyCVgMsVF9xuDEHgnLV40sBU5F9tTgop8Sg",
-    authDomain: "lam-dev-app.firebaseapp.com",
-    databaseURL: "https://lam-dev-app.firebaseio.com",
-    projectId: "lam-dev-app",
-    storageBucket: "lam-dev-app.appspot.com",
-    messagingSenderId: "609050148446",
-    appId: "1:609050148446:web:8ce2d1e5a10855be7bbecf",
-    measurementId: "G-FYCK2WJGZN"
+    apiKey: 'AIzaSyCVgMsVF9xuDEHgnLV40sBU5F9tTgop8Sg',
+    authDomain: 'lam-dev-app.firebaseapp.com',
+    databaseURL: 'https://lam-dev-app.firebaseio.com',
+    projectId: 'lam-dev-app',
+    storageBucket: 'lam-dev-app.appspot.com',
+    messagingSenderId: '609050148446',
+    appId: '1:609050148446:web:8ce2d1e5a10855be7bbecf',
+    measurementId: 'G-FYCK2WJGZN'
   }
 
 };
